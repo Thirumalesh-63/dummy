15817843d0267889d44d1d0a7d87a4a1339c4ca4
Thirumalesh-63 <129723705+Thirumalesh-63@users.noreply.github.com>
2024-08-12 13:56:47 +0530

    demo3

--- src/demo4/dummy3.java
+++ src/demo4/dummy3.java
@@ -1,10 +1,5 @@
 package demo4;
 
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.stream.Collectors;
-
 public class dummy3 {
 
 	
@@ -14,12 +9,5 @@ public class dummy3 {
 	
 	public static void main(String[] args) {
 		System.out.println("n");
-		  Set<Integer> strset=new HashSet<>();
-	        strset.add(1);
-	        strset.add(2);
-	        strset.add(14);
-	        strset.add(1);
-	        strset.stream().filter(l->l%2==0).sorted(Comparator.reverseOrder()).collect(Collectors.toList()).forEach(p->System.out.println(p));
-	        
 	}
 }
