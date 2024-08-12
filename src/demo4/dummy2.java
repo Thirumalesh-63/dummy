6ec03886de2716a04c0a49ba7f598592351c6d37
Thirumalesh-63 <129723705+Thirumalesh-63@users.noreply.github.com>
2024-08-12 13:39:48 +0530

    mybranchupdate

--- src/demo4/dummy2.java
+++ src/demo4/dummy2.java
@@ -1,5 +1,8 @@
 package demo4;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class dummy2 {
 
 	dummy2(){
@@ -7,5 +10,11 @@ public class dummy2 {
 	}
 	public static void main(String[] args) {
 		System.out.print("s");
+		
+		 Integer a[]= {1,2,3,4,3,5,2};
+			String s[]= {"hi","hello","namasthe"};
+			
+			Arrays.asList(a).stream().distinct().sorted(Comparator.reverseOrder()).forEach(w->System.out.println(w));
+		
 	}
 }
